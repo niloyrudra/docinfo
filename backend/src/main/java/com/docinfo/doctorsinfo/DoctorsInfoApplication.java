0b006772fd180eddbd9f5dcd1397c8ec24357cf5
Niloy Rudra <niloyrudra4249@gmail.com>
2023-02-16 09:10:17 +0600

    Initial commit

---
+++ backend/src/main/java/com/docinfo/doctorsinfo/DoctorsInfoApplication.java
@@ -0,0 +1,13 @@
+package com.docinfo.doctorsinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsInfoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/docinfo/doctorsinfo/DoctorsInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docinfo.doctorsinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
